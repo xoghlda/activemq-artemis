d5eb7530d9ac2f0ef348663d9f3ce56195e46e5f
Clebert Suconic <clebertsuconic@apache.org>
2022-05-16 14:17:15 -0400

    RA changes

--- artemis-ra/src/main/java/org/apache/activemq/artemis/ra/recovery/RecoveryManager.java
+++ artemis-ra/src/main/java/org/apache/activemq/artemis/ra/recovery/RecoveryManager.java
@@ -26,8 +26,12 @@ import org.apache.activemq.artemis.service.extensions.xa.recovery.ActiveMQRegist
 import org.apache.activemq.artemis.service.extensions.xa.recovery.ActiveMQRegistryImpl;
 import org.apache.activemq.artemis.service.extensions.xa.recovery.XARecoveryConfig;
 import org.apache.activemq.artemis.utils.collections.ConcurrentHashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class RecoveryManager {
+
+   private static final Logger logger = LoggerFactory.getLogger(RecoveryManager.class);
 
    private ActiveMQRegistry registry;
 
@@ -47,7 +51,7 @@ public final class RecoveryManager {
                                     String userName,
                                     String password,
                                     Map<String, String> properties) {
-      ActiveMQRALogger.LOGGER.debug("registering recovery for factory : " + factory);
+      logger.debug("registering recovery for factory : " + factory);
 
       XARecoveryConfig config = XARecoveryConfig.newConfig(factory, userName, password, properties);
       resources.add(config);
@@ -86,7 +90,7 @@ public final class RecoveryManager {
                registry = ActiveMQRegistryImpl.getInstance();
             }
          } catch (Throwable e) {
-            ActiveMQRALogger.LOGGER.debug("unable to load  recovery registry " + locatorClasse, e);
+            logger.debug("unable to load  recovery registry " + locatorClasse, e);
          }
          if (registry != null) {
             break;
@@ -94,7 +98,7 @@ public final class RecoveryManager {
       }
 
       if (registry != null) {
-         ActiveMQRALogger.LOGGER.debug("Recovery Registry located = " + registry);
+         logger.debug("Recovery Registry located = " + registry);
       }
    }
 
