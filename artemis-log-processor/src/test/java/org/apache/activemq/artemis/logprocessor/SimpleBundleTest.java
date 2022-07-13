a63c16e47c6ebe1edec01db471125e3347720894
Robbie Gemmell <robbie@apache.org>
2022-07-13 18:52:52 +0100

    fix test missed in prior change

--- artemis-log-processor/src/test/java/org/apache/activemq/artemis/logprocessor/SimpleBundleTest.java
+++ artemis-log-processor/src/test/java/org/apache/activemq/artemis/logprocessor/SimpleBundleTest.java
@@ -26,25 +26,25 @@ public class SimpleBundleTest {
 
    @Test
    public void testSimple() {
-      Assert.assertEquals("TST1 Test", SimpleBundle.MESSAGES.simpleTest());
+      Assert.assertEquals("TST1: Test", SimpleBundle.MESSAGES.simpleTest());
       System.out.println(SimpleBundle.MESSAGES.simpleTest());
    }
 
    @Test
    public void testParameters() {
-      Assert.assertEquals("TST2 V1-bb", SimpleBundle.MESSAGES.parameters(1, "bb"));
+      Assert.assertEquals("TST2: V1-bb", SimpleBundle.MESSAGES.parameters(1, "bb"));
    }
 
    @Test
    public void testException() {
       Exception ex = SimpleBundle.MESSAGES.someException();
-      Assert.assertEquals("TST3 EX", ex.getMessage());
+      Assert.assertEquals("TST3: EX", ex.getMessage());
    }
 
    @Test
    public void testSomeExceptionParameter() {
       String uuid = UUID.randomUUID().toString();
-      Assert.assertEquals(new Exception("TST4 EX-" + uuid).toString(), SimpleBundle.MESSAGES.someExceptionParameter(uuid).toString());
+      Assert.assertEquals(new Exception("TST4: EX-" + uuid).toString(), SimpleBundle.MESSAGES.someExceptionParameter(uuid).toString());
    }
 
    @Test
@@ -66,20 +66,20 @@ public class SimpleBundleTest {
       Exception myCause = new Exception("this is myCause");
       String logRandomString = "" + System.currentTimeMillis();
       MyException myException = SimpleBundle.MESSAGES.someExceptionWithCause(logRandomString, myCause);
-      Assert.assertEquals("TST8 EX" + logRandomString, myException.getMessage());
+      Assert.assertEquals("TST8: EX" + logRandomString, myException.getMessage());
       Assert.assertSame(myCause, myException.getCause());
    }
 
    @Test
    public void testABCD() {
       System.out.println(SimpleBundle.MESSAGES.abcd("A", "B", "C", "D"));
-      Assert.assertEquals("TST9 A B C D", SimpleBundle.MESSAGES.abcd("A", "B", "C", "D"));
+      Assert.assertEquals("TST9: A B C D", SimpleBundle.MESSAGES.abcd("A", "B", "C", "D"));
    }
 
    @Test
    public void testObjectsABCD() {
       System.out.println(SimpleBundle.MESSAGES.abcd("A", "B", "C", "D"));
-      Assert.assertEquals("TST10 A B C D", SimpleBundle.MESSAGES.objectsAbcd(new MyObject("A"), new MyObject("B"), new MyObject("C"), new MyObject("D")));
+      Assert.assertEquals("TST10: A B C D", SimpleBundle.MESSAGES.objectsAbcd(new MyObject("A"), new MyObject("B"), new MyObject("C"), new MyObject("D")));
    }
 
 
